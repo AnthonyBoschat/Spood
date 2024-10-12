3e7f6552f4d1b845f13826a53311db86db71ad95
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2024-10-12 02:38:52 +0200

    Premier style, importe du svg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import Home from "@Pages/Home/Home";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import './App.scss'
+import Header from "@Containers/Header/Header";
 
 
 export default function App() {
 
   
   return (
-        
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-    </Routes>
+      <>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </>
   )
 }
 
---
+++ Frontend/src/Containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss"
+
+export default function Header(){
+
+    return(
+        <header>
+            <nav>
+                <a href="/Nutrition">Nutrition</a>
+                <a href="/Sport">Sport</a>
+            </nav>
+        </header>
+    )
+}
