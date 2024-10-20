c95280949ea75b0711af3f5000d853d0ce9ce04e
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2024-10-20 20:22:25 +0200

    Ajustement des effets de transition

--- Frontend/src/Pages/Nutrition/Nutrition.jsx
+++ Frontend/src/Pages/Nutrition/Nutrition.jsx
@@ -7,20 +7,20 @@ export default function Nutrition(){
 
     return(
         <div className="nutrition-container">
-                <div className="module action">
-
+                <div className="module transition action">
+                    Action
                 </div>
 
                 <div className={`module calendar ${darkTheme()}`}>
-
+                    Calendar
                 </div>
 
                 <div className={`module ingredient-list ${darkTheme()}`}>
-
+                    Ingredient List
                 </div>
 
-                <div className="module recipe-list">
-
+                <div className="module transition recipe-list">
+                    Recipe List
                 </div>
         </div>
     )
